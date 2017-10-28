a78bc978b7c7100405b910bb31f8077a38420798
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2017-10-28 17:43:31 +0300

    Fix secret vars (probably x8)

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_4940b5ffd765_key -iv $encrypted_4940b5ffd765_iv -in .travis/signingkey.asc.enc -out .travis/signingkey.asc -d
-    gpg --fast-import .travis/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_4940b5ffd765_key -iv $encrypted_4940b5ffd765_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    gpg --fast-import .travis/codesigning.asc
 fi
