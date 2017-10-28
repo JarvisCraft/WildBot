9a266ec1021b7ac6e84a8d1227a9184273eacd60
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2017-10-28 16:07:00 +0300

    Add some debug

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    echo "Starting deployment to Sonatype"
     mvn deploy -P sign,build-extras --settings .travis/mvnsettings.xml
+    echo "Deployment to Sonatype has finished"
 fi
