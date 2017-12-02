53355b87cf195edcf50030e4feb25d1341c2b1a8
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2017-12-03 00:36:52 +0300

    Add final

--- src/main/java/ru/wildbot/wildbotcore/WildBotCore.java
+++ src/main/java/ru/wildbot/wildbotcore/WildBotCore.java
@@ -80,7 +80,7 @@ public class WildBotCore {
     // Main method
     ///////////////////////////////////////////////////////////////////////////
     
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         final Instant beginTime = Instant.now();
 
         // Tracer initialisation
