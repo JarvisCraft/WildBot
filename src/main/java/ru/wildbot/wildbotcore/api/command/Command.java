b0afe4bb88cc6138b0aba35e4047c262e3281d7b
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2017-12-03 21:24:08 +0300

    Continue writing JavaDocs

--- src/main/java/ru/wildbot/wildbotcore/api/command/Command.java
+++ src/main/java/ru/wildbot/wildbotcore/api/command/Command.java
@@ -21,13 +21,46 @@ import lombok.*;
 import java.util.Collection;
 import java.util.HashSet;
 
+/**
+ * A POJO representing a command used by {@link CommandManager}.
+ * @see CommandManager
+ */
 @Builder
 @EqualsAndHashCode
 public class Command {
+    /**
+     * Names by which the command is being registered and accessed.
+     */
     @Singular @NonNull @Getter private Collection<String> names;
+
+    /**
+     * Plugin name to be used for identifying this command.
+     * Empty value ({@code ""}) means that there's no plugin associated with this command,
+     * e.g. if this command is one of WildBot's default commands.
+     */
     @NonNull @Getter @Builder.Default private String pluginName;
+
+    /**
+     * If set to true then this command cannot be overridden.
+     * E.g. {@link CommandManager#register(Command)} will check this flag when being called.
+     * TODO REregister
+     */
     @Getter @Builder.Default private boolean locked;
+
+    /**
+     * An executor used by this command to perform the very action
+     */
     @NonNull @Getter private CommandExecutor executor;
+
+    /**
+     * Optional description for this command.
+     * E.g. used with {@link ru.wildbot.wildbotcore.core.command.DefaultCommand#HELP}
+     */
     @NonNull @Getter @Builder.Default private String description;
+
+    /**
+     * Optional description for this command's usage.
+     * E.g. used with {@link ru.wildbot.wildbotcore.core.command.DefaultCommand#HELP}
+     */
     @Getter @Builder.Default private String usage;
 }
--- src/main/java/ru/wildbot/wildbotcore/api/command/CommandExecutor.java
+++ src/main/java/ru/wildbot/wildbotcore/api/command/CommandExecutor.java
@@ -18,7 +18,25 @@ package ru.wildbot.wildbotcore.api.command;
 
 import java.util.List;
 
+/**
+ * A self-descriptive functional interface (it's okay to create it using lambdas or method references if it's simple *)
+ * which handles the given command according to it's input.
+ * It's also worth mentioning that due to it's recursive nature
+ * you can actually edit the very {@link Command} which is associated with this executor at the very time it's called.
+ *
+ * <p>(((* or if you are Dolya)))
+ *
+ * @see CommandManager
+ */
 @FunctionalInterface
 public interface CommandExecutor {
+    /**
+     * The only required method of this {@link FunctionalInterface} which is being called.
+     * @param command the command which is associated with this executor and which was called.
+     * @param name the name by which the command was called (also may be called alias).
+     * @param arguments the list of arguments which followed the command
+     *                  (in {@link CommandManager} they are split by regex's `\s`
+     * @return nullable runnable which may (but might not) be called after the command's execution
+     */
     Runnable execute(Command command, String name, List<String> arguments);
 }
